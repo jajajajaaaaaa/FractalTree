b1d8e6cbf47a8373394dc850fcc24a27f3ce4c14
Mariam Siradze <51333230+mariam-siradze@users.noreply.github.com>
2020-08-20 22:37:28 +0400

    Add files via upload

---
+++ FractalOrRecursionTree/FractalOrRecursionTree/FractalOrRecursionTree/FractalOrRecursionTree.py
@@ -0,0 +1,22 @@
+import turtle
+
+hr = turtle.Turtle()
+hr.left(90)
+hr.speed(650)
+hr.color("#AC3B61")
+turtle.bgcolor("#EEE2DC")
+
+def tree(i):
+    if i < 10:
+        return
+    else:
+        hr.forward(i)
+        hr.left(30)
+        tree(3*i/4)
+        hr.right(60)
+        tree(3*i/4)
+        hr.left(30)
+        hr.backward(i)
+
+tree(100)
+turtle.done()
